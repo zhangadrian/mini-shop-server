631d15c292b346116566c60aa6e99d4b39909441
adhcczhang <adhcczhang@tencent.com>
2020-07-15 20:23:02 +0800

    Add the remark and the group info.

--- app/model_views/group.py
+++ app/model_views/group.py
@@ -6,15 +6,18 @@ __author__ = "adhcczhang"
 
 
 class BaseGroupView(ModelView):
-    fields = ["group_name", "user_nickname", "shop_owner_nickname", "group_id", "poi_id"]
+    fields = ["group_name", "user_nickname", "shop_owner_nickname", "group_id", "poi_id", "shop_info"]
 
-    def __init__(self, group, shop_info):
+    def __init__(self, group, shop_info_dict):
         self.group_name = group.group_name
         self.user_nickname = group.user_nickname
         self.shop_owner_nickname = group.shop_owner_nickname
         self.group_id = group.group_id
         self.poi_id = group.poi_id
-        self.shop_info = shop_info
+        if self.poi_id in shop_info_dict:
+            self.shop_info = shop_info_dict[self.poi_id]
+        else:
+            self.shop_info = {}
 
 
 class GroupCollection:
@@ -28,13 +31,14 @@ class GroupCollection:
         shop_data_list = Shop.get_shop_info_list(shop_id_list)
         base_shop_collection = BaseShopCollection()
         shop_info_list = base_shop_collection.fill(shop_data_list)
-        return shop_info_list
+        shop_info_dict = {}
+        for shop_info in shop_info_list:
+            shop_info_dict[shop_info.poi_id] = shop_info
+        return shop_info_dict
 
     def fill(self, group_list):
-        shop_info_list = self.get_shop_list(group_list)
-        print(shop_info_list)
-        zip_group_shop = zip(group_list, shop_info_list)
-        self.items = [BaseGroupView(group_item, shop_item) for group_item, shop_item in zip_group_shop]
+        shop_info_dict = self.get_shop_list(group_list)
+        self.items = [BaseGroupView(group_item, shop_info_dict) for group_item in group_list]
         return self.items
 
 
