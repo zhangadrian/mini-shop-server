23e15646399c268462ecdc94b531292a63dc96a8
adhcczhang <adhcczhang@tencent.com>
2020-08-11 21:26:30 +0800

    Debug.

--- db_operation.py
+++ db_operation.py
@@ -10,11 +10,9 @@ db_user = 'root'
 db_pass = 'root'
 
 
-db = MySQLdb.connect(host=db_ip, port=int(db_port), db=db_name, user=db_user, passwd=db_pass)
-cursor = db.cursor()
-
-
 def update_shop_owner(poi_id_list, user_id_list):
+    db = MySQLdb.connect(host=db_ip, port=int(db_port), db=db_name, user=db_user, passwd=db_pass)
+    cursor = db.cursor()
     vals = list(zip(poi_id_list, user_id_list))
     sql = "update shop_info set user_id= %s where poi_id= %s"
     cursor.executemany(sql, vals)
